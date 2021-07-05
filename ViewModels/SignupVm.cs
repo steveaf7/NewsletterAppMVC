869482255d41cbed78dd6efd85ced14737af2372
StevenFralix <steveaf7@gmail.com>
2021-07-05 14:57:38 -0700

    Add project files.

---
+++ ViewModels/SignupVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsletterAppMVC.ViewModels
+{
+    public class SignupVm
+    {
+        // in a view model, you only need to pass what you need for the view. 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
